a436b0ce46e4f9df3ec5f40352bf9481cd643a6f
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-01-14 17:38:52 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
